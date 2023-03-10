329e76d61b0ed82bf0d24360afb0936d033a74b9
marudengi <dame95@naver.com>
2023-03-10 18:04:29 +0900

    add password encryption

--- src/main/java/hello/loginpractice/config/SecurityConfig.java
+++ src/main/java/hello/loginpractice/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +16,7 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
--- src/main/java/hello/loginpractice/controller/LoginController.java
+++ src/main/java/hello/loginpractice/controller/LoginController.java
@@ -2,6 +2,8 @@ package hello.loginpractice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.source.ConfigurationPropertyName.Form;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -35,9 +37,12 @@ public class LoginController {
 	
 	@RequestMapping("/register")
 	public String loginSuccess(@ModelAttribute User form) {
-				
+								
 		user.setName(form.getName());
-		user.setPassword(form.getPassword());
+		String rawPw = form.getPassword();
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		
+		user.setPassword(passwordEncoder.encode(rawPw));
 		user.setUserid(form.getUserid());
 		
 		try {
@@ -50,6 +55,10 @@ public class LoginController {
 		return "redirect:/auth";
 	}
 	
+	@GetMapping("/accessDenied")
+	public String loginFailed() {
+		return "accessDenied";
+	}
 //	System.out.println(userid + ", " + username + ", " + password);
 //	@RequestParam("userid") String userid,
 //	@RequestParam("username") String username,
